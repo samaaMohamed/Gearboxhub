d627cd4e83f8bb648763a04d8816761bb369cbf3
sama <sama@DESKTOP-PMTNS6L>
2021-12-18 13:39:34 +0200

    Edits

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
     function openNav() {
         document.getElementById("mySidenav").style.width = "100%";
+        document.getElementById("myHeader").style.backgroundColor = "#fff";
     }
 
     function closeNav() {
         document.getElementById("mySidenav").style.width = "0";
+        document.getElementById("myHeader").style.backgroundColor = "transparent";
     }
