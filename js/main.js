28c28ce44779d6b3a4656d1cd82b5320153fcfce
sama <sama@DESKTOP-PMTNS6L>
2021-12-18 14:19:33 +0200

    Edits

--- js/main.js
+++ js/main.js
@@ -1,9 +1,13 @@
     function openNav() {
         document.getElementById("mySidenav").style.width = "100%";
         document.getElementById("myHeader").style.backgroundColor = "#fff";
+        document.getElementById("openNav").style.display = "none";
+        document.getElementById("closeBtn").style.display = "block";
     }
 
     function closeNav() {
         document.getElementById("mySidenav").style.width = "0";
         document.getElementById("myHeader").style.backgroundColor = "transparent";
+        document.getElementById("openNav").style.display = "block";
+        document.getElementById("closeBtn").style.display = "none";
     }
